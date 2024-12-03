71eaa9d4a3b05cbba7652c8910d5dafba51ae2e7
Burak <iletisimburakaltinisik@outlook.com>
2024-12-03 20:38:52 +0300

    some jobs

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-import streamlit as st
 import pandas as pd
-import seaborn as sns
+from assets.PostgresManager import PostgresModel
 
-fdf = pd.read_csv('A1CAP.csv')
-# make a seaborn 2 lines for Real and Pred cols
-fdf.plot(figsize=(30,10))
+
 
--- assets/LSTMModeller.py
+++ assets/LSTMModeller.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
-import matplotlib.pyplot as plt
 import tensorflow
 from datetime import date
 from datetime import timedelta
